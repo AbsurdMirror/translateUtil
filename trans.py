de7f6f971ae42db91932828c2ca0cab841624a3d
dzx <dzx2552@163.com>
2024-03-12 10:45:52 +0800

    添加保存功能

--- trans.py
+++ trans.py
@@ -1,17 +1,24 @@
 from pynput.keyboard import Controller, Key, Listener 
-import traceback
-import time
 import pyperclip
 import json
 import requests
-import re  
+import os  
+
+###### 全局变量 ######
+ctrl_c_times = 0
+exit_progress = False
+
+hasSetting = False
+setting = {}
+
+f = open("CaiYunXiaoYi.token")
+CaiYunXiaoYiToken = f.read()
 
 def tranlateCaiYunXiaoYi(source, direction):
     url = "http://api.interpreter.caiyunai.com/v1/translator"
 
     # WARNING, this token is a test token for new developers,
     # and it should be replaced by your token
-    token = "??????"
 
     payload = {
         "source": source,
@@ -22,41 +29,22 @@ def tranlateCaiYunXiaoYi(source, direction):
 
     headers = {
         "content-type": "application/json",
-        "x-authorization": "token " + token,
+        "x-authorization": "token " + CaiYunXiaoYiToken,
     }
 
     response = requests.request("POST", url, data=json.dumps(payload), headers=headers)
 
     return json.loads(response.text)["target"]
-  
 
 def split_string_by_english_sentences(input_string):  
     # 将换行符替换为空格  
     input_string_no_newline = input_string.replace('\r', ' ')
     input_string_no_newline = input_string_no_newline.replace('\n', ' ')
 
-    # # 匹配英文句子结束的标志，但排除缩写后的点  
-    # sentence_endings = r"""  
-    #     (?<=[.!?])                # 确保点、问号或感叹号前面有字符（避免缩写）  
-    #     (?<!\w\.\w\.)             # 避免诸如 "Mr." 或 "Dr." 这样的缩写  
-    #     [.!?]+                     # 一个或多个点、问号或感叹号  
-    #     (?=\s+|$)                  # 确保后面有空白或字符串结束  
-    # """  
-    # sentence_endings = re.compile(sentence_endings, re.VERBOSE | re.IGNORECASE)  
-      
-    # # 使用re模块的split方法按照模式拆分字符串  
-    # sentences = sentence_endings.split(input_string_no_newline)  
     import nltk
     sentences = nltk.sent_tokenize(input_string_no_newline)
 
-    # # 移除拆分后数组中的空字符串，并去除句子两端的空白  
-    # sentences = [s.strip() for s in sentences if s.strip()]  
-      
     return input_string_no_newline, sentences
-
-# 全局变量，用于存储按下ctrl + d键的次数  
-ctrl_c_times = 0
-exit_progress = False
   
 def on_press(key):
     global ctrl_c_times
@@ -78,14 +66,27 @@ def on_press(key):
         tranlate_source_formatted = json.dumps(tranlate_source, indent=4, ensure_ascii=False)
         tranlate_target_formatted = json.dumps(tranlate_target, indent=4, ensure_ascii=False)  
 
+        os.system('cls')
         print(f"剪切板内容: {clipboard_content} \n")
         print(f"去除换行: {no_newline_content} \n")
         print(f"句子拆分: {tranlate_source_formatted} \n")
-        print(f"翻译结果: {tranlate_target_formatted} \n")\
+        print(f"翻译结果: {tranlate_target_formatted} \n")
         # print(f"Clipboard content: {clipboard_content} \n")
         # print(f"No newline content: {no_newline_content} \n")
         # print(f"Translation source: {tranlate_source_formatted} \n")
         # print(f"Translation target: {tranlate_target_formatted} \n")
+
+        if hasSetting :
+            if setting["save"]["isSave"]:
+                fileDirpath = os.path.join("./save", setting["save"]["savePath"])
+                if not os.path.exists(fileDirpath):  
+                    os.makedirs(fileDirpath)
+                filePath = os.path.join(fileDirpath, setting["save"]["saveName"])
+                with open(filePath, "a", encoding="utf-8") as savefile:
+                    for item in tranlate_target:
+                        # 将每一项写入文件，并在每一项后添加一个换行符  
+                        savefile.write(item + '\n')
+                    savefile.write("---\n")
 
         ctrl_c_times = 0
     
@@ -99,6 +100,24 @@ def on_release(key):
     global ctrl_pressed  
     if key == Key.ctrl:  
         ctrl_pressed = 0  
+
+
+############################################################################################################
+
+############################################
+# 设置文件名  
+filename = 'setting.json'  
+
+# 检查文件是否存在  
+if os.path.exists(filename):  
+    # 打开文件并读取内容  
+    with open(filename, 'r') as file:  
+        # 解析JSON内容为对象/字典
+        setting = json.load(file)
+        hasSetting = True
+else:  
+    print(f"文件 {filename} 不存在")
+############################################
 
 # 使用with语句来确保Listener正确关闭  
 with Listener(on_press=on_press, on_release=on_release) as listener:  
